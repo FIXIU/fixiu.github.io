fdfc2e6a62df73c639ee6ba6921fd322b218f0fb
FIXIU <fixiu@tuta.io>
2024-12-11 17:14:10 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://fixiu.github.io',
+    base: '/fixiu.github.io',
+});
