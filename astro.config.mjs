00c0cd64b5cf065899bd69ddd9e564396838ac7d
FIXIU <fixiu@tuta.io>
2025-01-30 23:53:42 +0100

    Many changes to wielomiany

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fixiu.github.io',
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
 });
