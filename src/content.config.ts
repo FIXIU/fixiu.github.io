3f44bb975ba12c1039a94dd37f6edc26fab413a9
FIXIU <fixiu@tuta.io>
2025-03-21 23:47:34 +0100

    feature updates

---
+++ src/content.config.ts
@@ -0,0 +1,9 @@
+import { defineCollection, z } from 'astro:content';
+
+import { glob, file } from 'astro/loaders';
+
+const blog = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
+});
+
+export const collections = { blog };
