2fa3f7667fea45fe8a828cda210f9eb12763c687
FIXIU <fixiu@tuta.io>
2026-01-08 16:44:44 +0100

    test1

--- app/[locale]/home-view.tsx
+++ app/[locale]/home-view.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import Magnetic from "../components/magnetic";
-import AnimatedLetters from "../components/animated-letters";
 import Marquee from "../components/marquee";
 
 type Props = {
@@ -15,18 +13,7 @@ export default function HomeView({ introText }: Props) {
       {/* Main Hero Section */}
       <div className="flex flex-1 flex-col items-center justify-center px-4">
         <div className="flex flex-col items-center justify-center z-10 gap-8 sm:gap-12">
-          {/* Massive Title with Staggered Animation */}
           <div className="flex flex-col items-center leading-none select-none">
-            <Magnetic>
-              <h1 className="text-[13vw] sm:text-[11vw] font-black text-zinc-900 dark:text-zinc-50 tracking-tighter hover:text-lime-400 transition-colors duration-300 cursor-default">
-                <AnimatedLetters text="FILIP" delay={0.2} />
-              </h1>
-            </Magnetic>
-            <Magnetic>
-              <h1 className="text-[13vw] sm:text-[11vw] font-black text-zinc-900 dark:text-zinc-50 tracking-tighter hover:text-lime-400 transition-colors duration-300 cursor-default">
-                <AnimatedLetters text="KOWALCZYK" delay={0.4} />
-              </h1>
-            </Magnetic>
           </div>
 
           {/* Intro Text */}
